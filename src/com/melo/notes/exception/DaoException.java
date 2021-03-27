9448e93daf22b281f724bdbb775a340163dd04ad
Melo <1158280627@qq.com>
2021-03-27 12:37:23 +0800

    Dao层异常

---
+++ src/com/melo/notes/exception/DaoException.java
@@ -0,0 +1,27 @@
+package com.melo.notes.exception;
+
+/**
+ * @author Jun
+ * @description Dao层异常
+ * @data 2021-3-27 10:30
+ */
+public class DaoException extends RuntimeException {
+    public DaoException() {
+    }
+
+    public DaoException(String message) {
+        super(message);
+    }
+
+    public DaoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DaoException(Throwable cause) {
+        super(cause);
+    }
+
+    public DaoException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
