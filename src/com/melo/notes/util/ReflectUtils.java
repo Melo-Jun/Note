f573bea6f68bdd8d55bb8abb24d7284d40b12565
Melo <1158280627@qq.com>
2021-03-27 12:21:59 +0800

    反射工具类

---
+++ src/com/melo/notes/util/ReflectUtils.java
@@ -0,0 +1,41 @@
+package com.melo.notes.util;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/**
+ * @author Jun
+ * @program StudentSystem
+ * @description 用于反射的工具
+ */
+public class ReflectUtils {
+
+
+    public static LinkedList<Method> getMethods(Object obj) {
+        return getMethods(obj.getClass());
+    }
+
+    public static LinkedList<Method> getMethods(Class clazz) {
+        LinkedList<Method> methods = new LinkedList<>();
+        for (Class cla = clazz; cla != Object.class; cla = cla.getSuperclass()) {
+            methods.addAll(Arrays.asList(cla.getDeclaredMethods()));
+        }
+        return methods;
+    }
+
+    public static LinkedList<Field> getFields(Object obj) {
+        return getFields(obj.getClass());
+    }
+
+    public static LinkedList<Field> getFields(Class clazz) {
+        LinkedList<Field> fields = new LinkedList<>();
+        for (Class cla = clazz; cla != Object.class; cla = cla.getSuperclass()) {
+            fields.addAll(Arrays.asList(cla.getDeclaredFields()));
+        }
+        return fields;
+    }
+
+
+}
